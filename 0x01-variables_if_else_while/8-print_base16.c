f8a27458bf793694451314c8baabd0cf0531889d
Bilqees Onoade <bilqeesonoade@gmail.com>
2023-02-20 12:42:02 -0800

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
